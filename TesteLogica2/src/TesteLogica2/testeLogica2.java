7516b5a138e7a0f891fc1b131ad493fa08695a66
Felipe Osorio <felipe.g.osorio@gmail.com>
2020-01-21 00:48:06 -0300

    Add files via upload

--- TesteLogica2/src/TesteLogica2/testeLogica2.java
+++ TesteLogica2/src/TesteLogica2/testeLogica2.java
@@ -5,38 +5,21 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class testeLogica2 {
-	Timer timer;
-	Double tempoTotal = 0.0;
 
-	public testeLogica2() {
-	    timer = new Timer();
-	    timer.schedule(new CalculadoraDecaimento(),
-	                   0,        //initial delay
-	                   1*30000);  //subsequent rate
-	    System.out.println(tempoTotal);
-	    	    
-	}
-
-	class CalculadoraDecaimento extends TimerTask {
-	    Double massaMinima = 0.10;
-	    Scanner s = new Scanner(System.in);
-	
+	public static void main(String args[]) {
+		Double massaMinima = 0.1;
+		Scanner s = new Scanner(System.in);
+		Double tempoTotal = 0.0;
+		
+		System.out.println("Insira a massa do material:");
 		Double massa = s.nextDouble();
-	    
-	    public void run() {
-	        if (massa >= massaMinima) {
-	            massa -= massa * 0.25;
-	            tempoTotal += 30.0;
-	            System.out.println("Tempo total: " + tempoTotal);
-	        } else {
-	          
-	            timer.cancel(); 
-	        }
-	    }
+		
+		while(massa >= massaMinima) {
+			tempoTotal += 30;
+			massa -= massa * 0.25;
+		}
+		System.out.println("Tempo total é de: " + tempoTotal + " segundos");
 	}
-	
-    public static void main(String args[]) {
-    System.out.println("Insira a massa do material.");
-        new testeLogica2();
-    }
+
+  
 }
